672d105fc60caa50fde0f739c68950ee31e5f1c5
ErikBurdett <erikaburdet@gmail.com>
2021-06-30 14:53:05 -0500

    added react-router-dom, created basic scaffolding with pages & components, will have to refractor and consolidate css into single file after finished - used TH site as reference & placeholder content. Next pages need to be built out - look into embedding podcast player rather than hosting

--- tome/src/App.js
+++ tome/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Podcast from './components/pages/Podcast';
+import Stories from './components/pages/Stories';
+import Signup from './components/pages/Signup';
+import Signin from './components/pages/Signin'
+import Tome from './components/pages/Tome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/stories' component={Stories} />
+          <Route path='/tome' component={Tome} />
+          <Route path='/tome' component={Podcast} />
+          <Route path='/signin' component={Signin} />
+          <Route path='/sign-up' component={Signup} />
+          
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ tome/src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css'
+import Footer from '../Footer';
+import HeroSection from '../HeroSection';
+
+
+
+function Home() {
+    return (
+       <>
+       <HeroSection/>
+       <Footer/>
+       </>
+    )
+}
+
+export default Home
